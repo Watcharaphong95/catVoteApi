586d0aeba51e5954dba714bc897606f5d8bfee81
Watcharaphong95 <65011212077@msu.ac.th>
2024-02-29 14:54:08 +0700

    Select date last time in the day

--- api/cat_picture.ts
+++ api/cat_picture.ts
@@ -4,6 +4,7 @@ import mysql from "mysql";
 
 export const router = express.Router();
 
+// Get picture all
 router.get("/", (req, res) => {
     const sql = 'select * from cat_picture';
 
@@ -12,3 +13,18 @@ router.get("/", (req, res) => {
         res.status(200).json(result);
     })
 })
+
+// Get picture where uid = xxxx
+router.get("/:uid", (req, res) => {
+    const uid = req.params.uid;
+
+    let sql = 'select * from cat_picture where p_uid = ?';
+    sql = mysql.format(sql, [
+        uid,
+    ]);
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        res.status(200).json({result});
+    })
+});
--- api/cat_record.ts
+++ api/cat_record.ts
@@ -4,11 +4,27 @@ import mysql from "mysql";
 
 export const router = express.Router();
 
+// Get all record
 router.get("/", (req, res) => {
-    const sql = 'select * from cat_pic_record';
+    const sql = 'select DATE_ADD(date, INTERVAL 7 HOUR) from cat_pic_record';
 
     conn.query(sql, (err, result) => {
         if(err) throw err;
         res.status(200).json(result);
     })
 })
+
+// Get record where pid and order by date all time
+router.get("/:pid", (req, res) => {
+    const pid = req.params.pid;
+    let sql = 'SELECT r_pid, MAX(DATE_ADD(date, INTERVAL 7 HOUR)) AS max_date FROM cat_pic_record WHERE r_pid = ? GROUP BY r_pid, DATE(date);';
+    sql = mysql.format(sql, [
+        pid,
+        pid,
+    ]);
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        res.status(200).json(result);
+    });
+});
